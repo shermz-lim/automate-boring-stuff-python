14aa1a00e56fec244a64942b6d4e1e3e9277d662
Sherman Lim <45409027+shermz-lim@users.noreply.github.com>
2019-02-20 20:55:52 +0800

    Update clicking.py

--- chapter-11-web/clicking.py
+++ chapter-11-web/clicking.py
@@ -1,10 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
-browser = webdriver.Chrome()
-browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
 
 email = input()
 password = input()
+
+browser = webdriver.Chrome()
+browser.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+
+
 
 usernameElem = browser.find_element_by_name('username')
 usernameElem.send_keys(email)
